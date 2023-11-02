13bc8cf32f6eb882e933d8dd55c87812e1bc84db
yixuan zhu <yixuanzhu@yixuans-MacBook-Pro.local>
2023-11-01 21:06:18 -0400

    added firebase

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -3,8 +3,15 @@ import 'package:project/page/CourseFlowCharPage.dart';
 import 'package:project/page/HomePage.dart';
 import 'package:project/page/LoginPage.dart';
 import 'package:project/page/courses_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+
   runApp(const MyApp());
 }
 
